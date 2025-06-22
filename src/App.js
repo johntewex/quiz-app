fc5461d724094ff958638a544bc3225926894b78
johntewex <62942621+johntewex@users.noreply.github.com>
2025-06-22 09:32:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -654,8 +654,9 @@ export default function QuizApp() {
   }
 
   return (
-    <div className="p-4 max-w-xl mx-auto text-center max-h-screen overflow-y-auto">
-      <h1 className="text-2xl font-bold mb-4">Fun with Friends</h1>
+    <div className="min-h-screen overflow-y-auto bg-white p-4 text-center">
+    <div className="max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">🎉Fun with Friends</h1>
 
       {stage === "category" && (
   <div className="category-selection">
